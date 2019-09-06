b490ecadd9e979db335e10ef5ebd85fcbf0a5c31
Mustafa OT <mustafa.ot@GLTMAC105.local>
2019-09-06 06:46:23 -0700

    mot first commit

---
+++ 11-NotificationCenter/CustomView.swift
@@ -0,0 +1,9 @@
+//
+//  CustomView.swift
+//  11-NotificationCenter
+//
+//  Created by Mustafa OT on 6.09.2019.
+//  Copyright © 2019 Mustafa OT. All rights reserved.
+//
+
+import Foundation
---
+++ 11-NotificationCenter/NotificationName.swift
@@ -0,0 +1,9 @@
+//
+//  NotificationName.swift
+//  11-NotificationCenter
+//
+//  Created by Mustafa OT on 6.09.2019.
+//  Copyright © 2019 Mustafa OT. All rights reserved.
+//
+
+import Foundation
